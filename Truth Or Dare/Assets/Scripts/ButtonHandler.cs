6c54c38bb0dc1872f5b34c4a06bc11a5c36ed564
TheLittleBro122 <mynameiscutepanda@gmail.com>
2021-01-18 00:42:23 +0800

    Created a simple Truth or Dare button for testing

---
+++ Truth Or Dare/Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonHandler : MonoBehaviour {
+
+    public Text resultText;
+
+    private Data data;
+
+    private void Start() {
+        data = FindObjectOfType<Data>();
+    }
+
+    public void Click(string tod) {
+        // tod = truth / dare
+
+        string result = data.getData(tod, "child");
+
+        resultText.text = "Your " + tod + " is \"" + result + "\"";
+    }
+
+}
